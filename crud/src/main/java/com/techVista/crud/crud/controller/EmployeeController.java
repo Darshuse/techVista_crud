a5eaf34ff7f4705b8e06e4dc7f484c66ca4f8173
MAhmed <MAhmed@ENEGDEV05.eastnets.com>
2022-02-03 10:13:53 +0200

    solve CORS issue By Adding filter allowe specific origins  to consume my api request

--- crud/src/main/java/com/techVista/crud/crud/controller/EmployeeController.java
+++ crud/src/main/java/com/techVista/crud/crud/controller/EmployeeController.java
@@ -4,10 +4,12 @@ import java.net.URI;
 import java.util.List;
 //import javax.validation.Valid;
 import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +22,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.techVista.crud.crud.entities.Employee;
 import com.techVista.crud.crud.exception.EmployeeNotFoundException;
 import com.techVista.crud.crud.service.EmployeeService;
-
+@CrossOrigin
 @RestController
 public class EmployeeController {
 	@Autowired
